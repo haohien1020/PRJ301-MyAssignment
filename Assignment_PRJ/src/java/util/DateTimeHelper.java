e1d07b7c15208fb655e11dd392e0f23a3c35a8ab
haohien1020 <108945232+haohien1020@users.noreply.github.com>
2022-10-28 19:45:50 +0700

    create more classs java
    create student , lecture , attendence , room, ...

--- Assignment_PRJ/src/java/util/DateTimeHelper.java
+++ Assignment_PRJ/src/java/util/DateTimeHelper.java
@@ -6,6 +6,7 @@ package util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -33,4 +34,81 @@ public class DateTimeHelper {
       
         
     }
+    public static java.util.Date toDateUntil(java.sql.Date d){
+        java.util.Date x = new java.util.Date(d.getTime());
+        x = removeTime(x);
+        return x;
+        
+        
+    }
+    public static java.util.Date toDateSql(java.util.Date d){
+        d= removeTime(d);
+        return new java.sql.Date(d.getTime());
+        
+        
+    }
+    public static int getDayofWeek(java.util.Date d){
+        Calendar c = Calendar.getInstance();
+        c.setTime(d);
+        int dayofWeek = c.get(Calendar.DAY_OF_WEEK);
+        return dayofWeek;
+    }
+    public static Date addDays(java.util.Date d , int days){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(d);
+        cal.add(Calendar.DATE, days);
+        return cal.getTime();
+        
+    }
+    public static ArrayList<java.sql.Date> 
+            getDateList(java.sql.Date from , java.sql.Date to){
+                ArrayList<java.sql.Date> dates = new ArrayList<>();
+                int days = 0;
+                java.util.Date e_from = toDateUtil(from);
+                java.util.Date e_to = toDateUtil(to);
+                while (true)
+                {
+                    java.util.Date d = DateTimeHelper.addDays(e_from, days);
+                    dates.add((java.sql.Date) toDateSql(d));
+                    days++;
+                    if(d.compareTo(e_to)>=0)
+                        break;
+                }
+                
+                
+                
+               return dates; 
+            }
+
+    private static Date toDateUtil(java.sql.Date from) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    public static String getDayNameofWeek(java.sql.Date s){
+        java.util.Date d = toDateUtil(s);
+        Calendar c = Calendar.getInstance();
+        c.setTime(d);
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        switch(dayOfWeek){
+            case 1 : return "Sun";
+            case 2 : return "Mon";
+            case 3 : return "Tue";
+            case 4 : return "Wed";
+            case 5 : return "Thu";
+            case 6 : return "Fri";
+            case 7 : return "Sat";
+            
+            
+        }
+        return "Error";
+    }
+    public static int compare(java.sql.Date a , java.sql.Date b){
+        Date e_a = toDateUtil(a);
+        Date e_b = toDateUtil(b);
+        System.out.println(a + "" + b +" " +e_a.compareTo(e_b));
+        return e_a.compareTo(e_b);
+    }
+
+    
 }
