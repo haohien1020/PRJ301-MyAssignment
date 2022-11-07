6bf5062325e66dd84e3ef2735c052c8fc3744ce0
haohien1020 <108945232+haohien1020@users.noreply.github.com>
2022-11-07 22:56:22 +0700

    fix login controller
    biết thế làm trc :))

--- Assignment_PRJ/src/java/controller/LoginController.java
+++ Assignment_PRJ/src/java/controller/LoginController.java
@@ -8,7 +8,6 @@ import dal.AccountDBContext;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,57 +17,15 @@ import model.Account;
  *
  * @author HP
  */
-@WebServlet(name = "LoginController", urlPatterns = {"/login"})
 public class LoginController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        AccountDBContext db = new AccountDBContext() {
-            @Override
-            public Account get(int id) {
-                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-            }
-        };
-        Account account = db.get(username, password);
-        if(account!=null)
-        {
-            request.getSession().setAttribute("account", account);
-            response.getWriter().println("login successful!");
-        }
-        else
-        {
-            response.getWriter().println("login failed!");
-        }
-        
-        
-    }
+   
     
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher("view/auth/login.jsp").forward(request, response);
     }
 
     /**
@@ -82,8 +39,26 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        AccountDBContext db = new AccountDBContext() {
+            @Override
+            public Account get(int id) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+        };
+        Account account = db.get(username, password);
+        if (account == null) {
+            response.getWriter().println("login failed!");
+        } else {
+            request.getSession().setAttribute("account", account);
+            response.getWriter().println("login successful!");
+            response.sendRedirect("/Asignment_PRJ/home");
+//            request.getRequestDispatcher("view/home.jsp").forward(request, response);
+        }
+
     }
+
 
     /**
      * Returns a short description of the servlet.
--- Assignment_PRJ/web/view/home.jsp
+++ Assignment_PRJ/web/view/home.jsp
@@ -9,16 +9,72 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>FAP</title>
     </head>
+    <style>
+        .left-header{
+            width: 555px;
+            border-color: #cccccc;
+            border-style: solid;
+            border-width: 1px;
+            color: #333333;
+            font-size: 13px;
+            line-height: 18.5714px;
+        }
+        .lhd{
+            background-color: #eb9316;
+            color: #ffffff;
+            width: 255px;
+            border-radius: 5px;
+            text-align: center;
+            display: block;
+        }
+        .greet{
+            width: 265px;
+            background-color: #5cb85c;
+            color: #ffffff;
+            display: inline;
+            /*text-align: center;*/
+            margin: 200px;
+            font-size: 25px;
+        }
+        .timetab{
+            border: 1px #333333 solid;
+            margin: 200px;
+            font-size: 25px;
+        }
+        .footer{
+            padding-top: 50px;
+            text-align: center;
+        }
+    </style>
     <body>
-     <c:if test="${sessionScope.account ne null}">
-            Hello ${sessionScope.account.displayname}, click
-            <a href="logout">here</a> to logout.
+        <h1 style="text-align: center;">FPT University Academic Portal</h1>
+        <hr>
+        <c:if test="${sessionScope.account ne null}">
+            <span class="timetab"><a href="http://localhost:9999/1_PRJ301_Assignment_HE161488/lecturer/timetable?lid=1">Time Table</a></span><br>
+            <div class="greet">
+                <span> Hello ${sessionScope.account.displayname}, click 
+                    <a id="myBtn" href="logout">here</a> to logout. </span>
+            </div>
         </c:if>
-            <c:if test="${sessionScope.account eq null}">
-            click
-            <a href="login">here</a> to login.
+        <div>
+
+        </div>
+        <c:if test="${sessionScope.account eq null}">
+            <div class="left-header">
+                <span class="lhd"> <b>PLEASE LOGIN BY FPT ACCOUNT</b></span> <br>
+                <div class="login"><button><a href="login"> Login</a></button></div>
+            </div>
+            <!--            click 
+                        <a href="login">here</a> to login. -->
         </c:if>
-    </body>
+        <p class="footer">© Powered by <a href="https://fpt.edu.vn/">FPT University</a> | <a href="https://cmshn.fpt.edu.vn/">CMS</a> | <a href="http://libol.fpt.edu.vn/">library</a> | <a href="https://library.books24x7.com/login.asp?ic=0">books24x7</a></p>
+    </div>
+    <script>
+        document.getElementById("myBtn").addEventListener("click", function () {
+            alert("Are you sure you want log out?");
+        });
+    </script>
+</body>
 </html>
